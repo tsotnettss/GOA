da6806a91888f93980fe5654b9023ae3b30caead
tsotnettss <145036712+tsotnettss@users.noreply.github.com>
2023-10-24 19:02:25 +0400

    Add files via upload

---
+++ GOA/day 9/homeworks/homework 6.py
@@ -0,0 +1,103 @@
+# #მოკლედ ჩემი მოფიქრებული დავალაება:  შექმენით 10 მათემატიკური შეკითხვა მომხმარებლისთვის მაგალითად
+# ( 67-43=?,  86+23=?  5*18=?  და ასეშემდეგ  .......  რომ პასუხები იყოს რიცხვები)  შემდეგ გააკეთეთ ისე რომ
+# ყოველი მცდარი ან  სწორი პასუხის შემდეგ პასუხის ქვემოთ ეწერებოდეს სწორია ან არასწორია ხოლო ტესტის დასრულების
+# შემდეგ თუ 10 ივე  კითხვის პასუხი იქნება ჭეშმარიტი დაიპრინტოს: "შენ აშკარად GOA ში სწავლობდი ჩემო ძმაო".   ხოლო
+# თუ ამ 10 პასუხიდან 1 მაინც იქნება მცდარი დაიპრინტოს ..."კიდევს სცადეთ ან შემოგვიერთდი GOA ში, GOA ელს ეს არეკადრება" 
+# ..... წარმატებები მეგობრებო   ❤️
+
+
+
+
+
+
+ans0=int(input("ამოცანა 1.... 72+96=?: "))
+
+if ans0== 168:
+    print("The answer is correct")
+else:
+    print("This answer is incorrect")
+
+
+
+ans1=int(input("ამოცანა 2.... 6*8=?: "))
+if ans1==48:
+    print("The answer is correct")
+else:
+    print("This answer is incorrect")
+
+
+
+
+ans2=int(input("ამოცანა 3..... 5 ხარისხად 3: "))
+if ans2==125:
+    print("The answer is correct")
+else:
+    print("This answer is incorrect")
+
+
+
+ans3=int(input("ამოცანა 4.... 100 ის 33 პროცენტი=?: "))
+
+if ans3==33 :
+    print("The answer is correct")
+else:
+    print("This answer is incorrect")
+
+
+
+ans4=int(input("ამოცანა 5.... სამკუთხედის კუთხეების ჯამრუი გრადსული ზომა ? : "))
+if ans4==180:
+    print("The answer is correct")
+else:
+    print("This answer is incorrect")
+
+
+
+
+ans5=int(input("ამოცანა 6.... 327-71=?: "))
+if ans5==256:
+    print("The answer is correct")
+else:
+    print("This answer is incorrect")
+
+
+
+ans6=int(input("ამოცანა 7.... (25+60)*3=?: "))
+
+if ans6==255 :
+    print("The answer is correct")
+else:
+    print("This answer is incorrect")
+
+
+
+ans7=int(input("ამოცანა 8.... 17+42+83=?: "))
+if ans7==142:
+    print("The answer is correct")
+else:
+    print("This answer is incorrect")
+
+
+
+
+ans8=int(input("ამოცანა 9..... (124/4)+17=?: "))
+if ans8==48:
+    print("The answer is correct")
+else:
+    print("This answer is incorrect")
+
+
+
+ans9=int(input("ამოცანა 10.... 69+69+69+69=?: "))
+if ans9==276:
+    print("The answer is correct")
+else:
+    print("This answer is incorrect")
+
+answers=[ans0,ans1,ans2,ans3,ans4,ans5,ans6,ans7,ans8,ans9]
+if answers[0]==168 and answers[1]==48 and answers[2]==125  and answers[3]==33 and answers[4]==180 and answers[5]==256 and answers[6]==255 and answers [7]==142 and answers [8]==48 and answers [9]==276 :
+    print("შენ აშკარად GOA ში სწავლობდი ჩემო ძმდაო... <3  <3  <3 ")
+else:
+    print("კიდევს სცადეთ ან შემოგვიერთდი GOA ში, GOA ელს ეს არეკადრება" )
+
+
