e8a818545fdc2f8df56555840c371a509b1a8aba
tsotnettss <145036712+tsotnettss@users.noreply.github.com>
2023-12-11 20:40:55 +0400

    Add files via upload

--- GOA/day 20/homeworks/homework.py
+++ GOA/day 20/homeworks/homework.py
@@ -114,3 +114,31 @@ def century(year):
 # Your task is to write a function which returns the time since midnight in milliseconds
 
 return ((h*3600000)+(m*60000)+(s*1000))
+
+
+# Abbreviate a Two Word Name
+# Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+# The output should be two capital letters with a dot separating them.
+
+def abbrev_name(name):
+    list=[]
+    x = name.split()
+    list.append(x)
+    element1=x[0]
+    element2=x[1]
+    elementfirst1=element1[0]
+    elementfirst2=element2[0]
+    return (elementfirst1.upper()+"."+elementfirst2.upper())
+
+
+# Opposites Attract
+# Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers 
+# has an even number of petals and the other has an odd number of petals it means they are in love.
+# Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+
+def lovefunc( flower1, flower2 ):
+    if flower1%2==1  and flower2 %2<1:
+        return True
+    elif flower1%2==0  and flower2 %2==1:
+        return True
+    return False
